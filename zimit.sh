500115b22b1f4333e4c33015cba208d9a958ef2d
Robert Manietta <robert.manietta@gmail.com>
2025-03-12 17:47:28 +1000

    Create zimit.sh
    Script to run docker instance of zimit and create a zim archive of a website for offline viewing.

---
+++ zimit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+printf "Zimit"
+printf "URL to Archive: " >&2
+read -r URL
+printf "Archive File Name: " >&2
+read -r file
+
+docker run  -v ~/www/zim:/output ghcr.io/openzim/zimit zimit --url $URL --name $file
