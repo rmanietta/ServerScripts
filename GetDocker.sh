2c7b66c2bdf32f01af415d7d5dbffae103e87ece
Robert Manietta <robert.manietta@gmail.com>
2025-03-13 08:26:15 +1000

    Update GetDocker.sh
    Updated with prompts and to install docker, docker-compose, docker in a rootless mode and incus if/as required.

--- GetDocker.sh
+++ GetDocker.sh
@@ -1,17 +1,38 @@
 #!/bin/sh
 
-#Install Docker
+## Script can install incus or docker and docker compose as well as setup docker in a rootless mode. Using prompts to complete for alpine linux server.
 
+#Install Docker
+printf 'Install Docker (y/n)? '
+read answer1
+
+if [ "$answer1" != "${answer1#[Yy]}" ]; then
 doas apk add docker
 doas rc-update add docker default
 doas service docker start
 doas addgroup ${USER} docker
+else 
+printf 'Setup docker rootless mode (y/n)? '
+read answer2
 
+if ["$answer2" != "${answer2#[Yy]}" ]; then
 # Docker rootless
 doas apk add docker-rootless-extras
 doas echo 'rc_cgroup_mode="unified"' >> /etc/rc.conf
 doas rc-update add cgroups
 doas service cgroups start
-
+else 
+printf 'Install Docker Compose (y/n)? '
+read answer3
+if ["$answer3" != "${answer3#[Yy]}" ]; then
 # Install Docker Compose
-apk add docker-cli-compose
+doas apk add docker-cli-compose
+printf 'Install Incus (y/n)? '
+read answer4
+if ["$answer4" != "${answer4#[Yy]}" ]; then
+# Install incus
+doas apk add incus
+else 
+ echo 'Nothing Installed '
+ exit
+ 
